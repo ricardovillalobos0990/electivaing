6c526ab864436b1477e6346d9805f5b98a77be18
Ricardo Andres Villalobos Marulanda <ricardovillalobos0990@gmail.com>
2021-05-19 10:56:15 -0500

    Segunda Parte del Proyecto

--- routes/people.js
+++ routes/people.js
@@ -1,20 +1,46 @@
-const { Router } = require("express");
+const { Router, request } = require("express");
 const router = Router();
 
+const people = [
+  {
+    id: "1",
+    firstname: "Ricardo",
+    lastname: "Villalobos",
+    gender: "F",
+    age: 30,
+  },
+  {
+    id: "2",
+    firstname: "Andres",
+    lastname: "Marulanda",
+    gender: "M",
+    age: 35,
+  },
+  {
+    id: "3",
+    firstname: "Steven",
+    lastname: "Florez",
+    gender: "M",
+    age: 35,
+  },
+];
+
 router.get("/", (req, res) => {
-  let people = [
-    {
-      firstname: "Ricardo",
-      lastname: "Villalobos",
-      age: 30,
-    },
-    {
-      firstname: "Andres",
-      lastname: "Marulanda",
-      age: 35,
-    },
-  ];
   res.json(people);
+});
+
+router.get("/male", (req, res) => {
+  let newPeople = people.filter(people => people.gender === "M")
+  res.json(newPeople);
+});
+
+router.get("/:id", (req, res) => {
+  let newPerson = people.find(person => person.id === req.params.id);
+  if(newPerson === undefined){
+    res.status(404).json({error: "Not Found"});
+  }else{
+    res.status(200).json(newPerson);
+  }
 });
 
 module.exports = router;
